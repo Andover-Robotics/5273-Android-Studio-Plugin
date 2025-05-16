796ea78b2a92a870e190813734b97cefa2a9d1cb
andrewcodes28 <180860358+andrewcodes28@users.noreply.github.com>
2025-05-16 17:19:49 -0400

    test

--- src/main/java/com/plugin/AddGradleDependency.java
+++ src/main/java/com/plugin/AddGradleDependency.java
@@ -2,27 +2,22 @@ package com.plugin;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.command.WriteCommandAction;
-import com.intellij.openapi.fileTypes.FileType;
-import com.intellij.openapi.fileTypes.FileTypeManager;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileManager;
-import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiFileFactory;
 import com.intellij.psi.PsiManager;
-
-import javax.swing.*;
+import org.jetbrains.plugins.groovy.lang.psi.GroovyFile;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.members.GrMethod;
 
 import java.nio.file.Paths;
-import java.util.Enumeration;
-import java.util.Objects;
 
-public class EditGradleFile extends AnAction {
+public class AddGradleDependency extends AnAction {
     private static PsiFile getGradleFile(Project project) {
 
         String fullPath = Paths.get(project.getBasePath(),"TeamCode/build.gradle").toString();
@@ -44,6 +39,18 @@ public class EditGradleFile extends AnAction {
         return psiFile;
     }
 
+    private static void replacePsiFile(Project project, PsiFile file, String text) {
+        Document document = FileDocumentManager.getInstance().getDocument(file.getVirtualFile());
+        if (document != null) {
+            WriteCommandAction.runWriteCommandAction(project, () -> {
+                document.setText(text);
+            });
+        }
+    }
+
+    // aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    private final String LOOKUP = "dependencies {";
+
     @Override
     public void actionPerformed(AnActionEvent e) {
         Project project = e.getProject();
@@ -55,11 +62,14 @@ public class EditGradleFile extends AnAction {
         if (gradleFile == null) {
             return;
         }
-        String gradleContents = gradleFile.getText();
-        if (gradleContents.isEmpty()) {
-            Messages.showErrorDialog(project, "Gradle file is empty.", "Error");
-            return;
-        }
-        Messages.showInfoMessage(gradleContents, "Gradle Contents:");
+
+        String text = gradleFile.getText();
+        int index = text.indexOf(LOOKUP);
+        if (index == -1) return;
+
+        String dep = Messages.showInputDialog("Dependency name:", "Add Gradle Dependency", null);
+        int realIndex = index + LOOKUP.length();
+        text = text.substring(0, realIndex) + "\n\timplementation \"" + dep + "\"" + text.substring(realIndex);
+        replacePsiFile(e.getProject(), gradleFile, text);
     }
 }
