4d255649387f939ae2087221309bcdda2d01bab9
andrewcodes28 <180860358+andrewcodes28@users.noreply.github.com>
2025-09-13 14:51:54 -0400

    fix adding libraries

--- src/main/java/com/plugin/AddGradleDependency.java
+++ src/main/java/com/plugin/AddGradleDependency.java
@@ -3,39 +3,28 @@ package com.plugin;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.command.WriteCommandAction;
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.ComboBox;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileManager;
-import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.ui.JBColor;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.groovy.lang.psi.GroovyFile;
 import org.jetbrains.plugins.groovy.lang.psi.GroovyPsiElementFactory;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrStatement;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.blocks.GrClosableBlock;
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrCall;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrMethodCall;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.path.GrMethodCallExpression;
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.params.GrParameterList;
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.members.GrMethod;
-import org.jetbrains.plugins.groovy.lang.resolve.api.Argument;
 import org.jetbrains.plugins.groovy.lang.resolve.api.GroovyMethodCallReference;
 
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -187,15 +176,6 @@ public class AddGradleDependency extends AnAction {
         return psiFile;
     }
 
-    private static void replacePsiFile(Project project, PsiFile file, String text) {
-        Document document = FileDocumentManager.getInstance().getDocument(file.getVirtualFile());
-        if (document != null) {
-            WriteCommandAction.runWriteCommandAction(project, () -> {
-                document.setText(text);
-            });
-        }
-    }
-
     private @Nullable GroovyMethodCallReference getReference(ArrayList<GroovyMethodCallReference> refs, String name) {
         for (GroovyMethodCallReference ref : refs) {
             if (ref.getMethodName().equals(name)) return ref;
@@ -230,38 +210,49 @@ public class AddGradleDependency extends AnAction {
         GroovyPsiElementFactory fac = GroovyPsiElementFactory.getInstance(e.getProject());
 
         GroovyMethodCallReference ref = getReference(refs, "dependencies");
+        GrClosableBlock block;
         GrStatement statement;
         if (ref == null) {
-            // Need to figure out how to extract a GroovyMethodCallReference from this one
-            statement = fac.createStatementFromText("dependencies {\n\n}");
-            ref = PsiTreeUtil.findChildrenOfType(statement, GrMethodCallExpression.class).iterator().next().getCallReference();
+            statement = fac.createStatementFromText("dependencies {\n}");
+            block = (GrClosableBlock) statement.getChildren()[2];
         } else {
             statement = null;
+            block = ((GrMethodCall) ref.getElement()).getClosureArguments()[0];
         }
 
         GroovyMethodCallReference ref2 = getReference(refs, "maven");
-        GrStatement statement2 = null;
+        GrClosableBlock block2; // maven block
+        GrStatement statement2; // maven statement
+        GrClosableBlock block3; // repositories block
+        GrStatement statement3; // repositories statement
 
-        // Same problem here
-        /*if (ref2 == null) {
+        if (ref2 == null) {
             GroovyMethodCallReference ref3 = getReference(refs, "repositories");
             if (ref3 == null) {
-                // uhhh
+                statement3 = fac.createStatementFromText("repositories {\n}");
+                block3 = (GrClosableBlock) statement3.getChildren()[2];
+            } else {
+                statement3 = null;
+                block3 = ((GrMethodCall) ref3.getElement()).getClosureArguments()[0];
             }
-            statement2 = fac.createStatementFromText("maven {\n\n}");
-            ref2 = PsiTreeUtil.findChildrenOfType(statement, GrMethodCallExpression.class).iterator().next().getCallReference();
-        }*/
-
-        GrClosableBlock block = ((GrMethodCall) ref.getElement()).getClosureArguments()[0];
-        GrClosableBlock block2 = ((GrMethodCall) ref2.getElement()).getClosureArguments()[0];
+            statement2 = fac.createStatementFromText("maven {\n}");
+            block2 = (GrClosableBlock) statement2.getChildren()[2];
+        } else {
+            statement3 = null;
+            block3 = null;
+            statement2 = null;
+            block2 = ((GrMethodCall) ref2.getElement()).getClosureArguments()[0];
+        }
 
         WriteCommandAction.runWriteCommandAction(project, () -> {
-            // TODO refine
             block2.addStatementBefore(fac.createStatementFromText("url = 'https://maven.brott.dev/'"), null);
             block2.addStatementBefore(fac.createStatementFromText("url = 'https://maven.pedropathing.com/'"), null);
             for (Library library: dialog.libraries) {
                 block.addStatementBefore(fac.createStatementFromText("implementation \"" + library.library + ":" + library.version + "\""), null);
             }
+            if (statement != null) file.addStatementBefore(statement, null);
+            if (statement2 != null) block3.addStatementBefore(statement2, null);
+            if (statement3 != null) file.addStatementBefore(statement3, null);
         });
     }
 }
